2cfd9b13341feca47af4d07527476360aad366c8
juanocoronel <138510581+juanocoronel@users.noreply.github.com>
2023-08-03 20:14:45 +0000

    cardgenerator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,53 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("hellou");
+
+  let iconsList = [
+    "bi-suit-heart-fill text-danger",
+    "bi-suit-diamond-fill text-danger",
+    "bi-suit-club-fill text-black",
+    "bi-suit-spade-fill text-black"
+  ];
+
+  let numberRandomList = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "A",
+    "K"
+  ];
+
+  let iconRandom = Math.floor(Math.random() * iconsList.length);
+  let numberRandom = Math.floor(Math.random() * numberRandomList.length);
+
+  // let topIcon1 = document.querySelectorAll("i")[0];
+  // topIcon1.classList.add(iconsList[iconRandom]);
+  // // console.log(">>>", topIcon1[1]);
+  // // console.log(">>>", topIcon1[2]);
+
+  // let topIcon2 = document.querySelectorAll("i")[1];
+  // topIcon2.classList.add(numberRandomList[numberRandom]);
+  // console.log(">>>", topIcon2[1]);
+  // console.log(">>>", topIcon2[2]);
+
+  // console.log(iconRandom);
+  // console.log(numberRandom);
+
+  document.getElementById("icon").className = iconsList[iconRandom];
+  document.getElementById("icon2").className = iconsList[iconRandom];
+
+  let centerIcon = document.getElementById("center-icon");
+  centerIcon.innerHTML = numberRandomList[numberRandom];
 };
+
+document.getElementById("btnRefresh").addEventListener("click", function() {
+  location.reload();
+});
